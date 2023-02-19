96a56a5dd727178383dcdb2832a475c4c6a8fc37
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-19 16:26:41 -0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Matheus Viana on 19/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
