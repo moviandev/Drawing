b3c3bd52dac629fbe68c53a23508c850fd7a99b3
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-19 16:57:25 -0300

    Using rounded borders we don't need to call closeSublinepath

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -14,9 +14,8 @@ struct ContentView: View {
             path.addLine(to: CGPoint(x: 100, y: 300))
             path.addLine(to: CGPoint(x: 300, y: 300))
             path.addLine(to: CGPoint(x: 200, y: 100))
-            path.closeSubpath()
         }
-        .stroke(.blue, lineWidth: 10)
+        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
     }
 }
 
