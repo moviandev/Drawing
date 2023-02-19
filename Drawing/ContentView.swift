2c6f472e27e3b72203743cefe4d11c159a19bade
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-19 16:58:23 -0300

    rollback

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,13 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-        }
-        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        Text("Hello, world!")
+            .padding()
     }
 }
 
