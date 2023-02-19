0ca134e662a1b98ba976f368d60b9c24a1364810
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-19 17:13:47 -0300

    rollback

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,42 +7,10 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        
-        return path
-    }
-}
-
-struct Arc: Shape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockWise: Bool
-    
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjusment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjusment
-        let modifiedEnd = endAngle - rotationAdjusment
-        
-        var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockWise)
-        
-        return path
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockWise: true)
-            .stroke(.blue, lineWidth: 10)
-            .frame(width: 300, height: 300)
+        Text("Hello, world!")
+            .padding()
     }
 }
 
