2af7e955cd2681afe3855eeedffa10e2025d2244
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-20 20:22:42 -0300

    Using imagePaint to render a image in the border of our elements

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
     }
 }
 
