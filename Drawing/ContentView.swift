be356c45d0d25d80052581b0295f1be78b7a8f40
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-20 20:40:33 -0300

    Using drawingGroup to render images with Metal, it should not be used so often.

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,10 +7,43 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps: Int = 100
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            ForEach(0..<steps, id: \.self) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(colors: [color(for: value, brightness: 1), color(for: value, brightness: 0.5)], startPoint: .top, endPoint: .bottom),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ContentView: View {
+    @State private var colorCycle = 0.0
+    
+    var body: some View {
+        ColorCyclingCircle(amount: colorCycle)
+            .frame(width: 300, height: 300)
+        
+        Slider(value: $colorCycle)
     }
 }
 
