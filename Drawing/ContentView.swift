abd66a1e86d1ab5d42051e3f2633545fe47039d6
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-20 19:51:26 -0300

    Rollback

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,31 +7,6 @@
 
 import SwiftUI
 
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockWise: Bool
-    var insetAmount = 0.0
-    
-    func path(in rect: CGRect) -> Path {
-        let rotationAdjusment = Angle.degrees(90)
-        let modifiedStart = startAngle - rotationAdjusment
-        let modifiedEnd = endAngle - rotationAdjusment
-        
-        var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockWise)
-        
-        return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
-    }
-}
-
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
