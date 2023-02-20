cca76b86ab91c16724188364466bec72c6ff78de
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-20 20:23:32 -0300

    rollback

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -9,9 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Capsule()
-            .strokeBorder(ImagePaint(image: Image("example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), lineWidth: 20)
-            .frame(width: 300, height: 200)
+        Text("Hello, world!")
+            .padding()
     }
 }
 
