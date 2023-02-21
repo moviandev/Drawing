4a68da2edf04ea350083d93fa443bb23be0cf61e
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-21 15:15:59 -0300

    rollback

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,38 +7,10 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: Double
-    
-    var animatableData: Double {
-        get { insetAmount }
-        set { insetAmount = newValue}
-    }
-    
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: 0, y: rect.maxY))
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
-        
-        return path
-    }
-}
-
-struct ContentView: View {
-    @State private var insetAmount = 50.0
-    
+struct ContentView: View { 
     var body: some View {
-        Trapezoid(insetAmount: insetAmount)
-            .frame(width: 200, height: 200)
-            .onTapGesture {
-                withAnimation {
-                    insetAmount = Double.random(in: 10...90)
-                }
-            }
+        Text("Hello, world")
+            .padding()
     }
 }
 
