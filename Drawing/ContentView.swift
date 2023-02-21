606af5cd38f26eedf4f1b8dfcfdaccb9b08a2aae
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-21 15:04:51 -0300

    rollback

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -11,20 +11,8 @@ struct ContentView: View {
     @State private var amount = 0.0
     
     var body: some View {
-        VStack {
-            Image("example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .saturation(amount)
-                .blur(radius: (1 - amount) * 20)
-            
-            Slider(value: $amount)
-                .padding()
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.black)
-        .ignoresSafeArea()
+        Text("Hello, world")
+            .padding()
     }
 }
 
