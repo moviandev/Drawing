4c7eda9d26a9ef008822a9e7f417ab60f5ed858c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-21 15:08:14 -0300

    create Trapezoid

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,6 +7,22 @@
 
 import SwiftUI
 
+struct Trapezoid: Shape {
+    var insetAmount: Double
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     @State private var amount = 0.0
     
