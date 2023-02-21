370b64dbbf32be0b45a24d39845b9cd7f2ca5b4d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-21 15:32:55 -0300

    Understanding how we can animate more than 1 property

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -11,6 +11,17 @@ struct Checkerboard: Shape {
     var rows: Int
     var columns: Int
     
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
